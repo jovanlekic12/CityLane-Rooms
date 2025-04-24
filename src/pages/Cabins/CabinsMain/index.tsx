f9bc4d6546f7384c555283337505a5a85d42cef6
jovanlekic12 <lekicjovan1212@gmail.com>
2025-04-24 22:24:02 +0200

    refactor code

--- src/pages/Cabins/CabinsMain/index.tsx
+++ src/pages/Cabins/CabinsMain/index.tsx
@@ -3,13 +3,14 @@ import { useFetchData } from "../../../API/useFetchData";
 import Button from "../../../components/Button";
 import { Cabin } from "../../../utils/types";
 import { TbDotsVertical } from "react-icons/tb";
+import Loader from "../../../components/Loader";
+
 function CabinsMain() {
   const infos = ["", "Cabin", "Capacity", "Price", "Discount", ""];
 
-  const { data: cabins, error, isLoading } = useFetchData<Cabin>(fetchCabins);
+  const { data: cabins, isLoading } = useFetchData<Cabin>(fetchCabins);
 
   console.log(cabins);
-
   return (
     <div className="section__main">
       <header className="section__main__header">
@@ -17,30 +18,36 @@ function CabinsMain() {
           return <div className="info__div">{info}</div>;
         })}
       </header>
-      <ul className="section__list">
-        {cabins.map((cabin) => (
-          <li key={cabin.id} className="section__list__item">
-            <img src={cabin.img} alt="cabin photo" className="cabin__img" />
-            <div className="cabin__number">
-              {String(cabin.id).padStart(3, "0")}
-            </div>
-            <div className="cabin__capacity">
-              Fits up to {cabin.capacity} guests
-            </div>
-            <div className="cabin__price">${cabin.price.toFixed(2)}</div>
-            {cabin.discount ? (
-              <div className="cabin__discount">
-                ${cabin.discount.toFixed(2)}
-              </div>
-            ) : (
-              <div>-</div>
-            )}
-            <Button type="options">
-              <TbDotsVertical></TbDotsVertical>
-            </Button>
-          </li>
-        ))}
-      </ul>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ul>
+          {cabins.map((cabin) => {
+            return (
+              <li key={cabin.id} className="section__list__item">
+                <img src={cabin.img} alt="cabin photo" className="cabin__img" />
+                <div className="cabin__number">
+                  {String(cabin.id).padStart(3, "0")}
+                </div>
+                <div className="cabin__capacity">
+                  Fits up to {cabin.capacity} guests
+                </div>
+                <div className="cabin__price">${cabin.price.toFixed(2)}</div>
+                {cabin.discount ? (
+                  <div className="cabin__discount">
+                    ${cabin.discount.toFixed(2)}
+                  </div>
+                ) : (
+                  <div>-</div>
+                )}
+                <Button type="options">
+                  <TbDotsVertical></TbDotsVertical>
+                </Button>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </div>
   );
 }
---
+++ src/utils/helpers.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+
+export function useOutsideClick(
+  ref: React.RefObject<HTMLElement>,
+  handler: () => void
+) {
+  useEffect(() => {
+    const listener = (e: MouseEvent) => {
+      if (!ref.current || ref.current.contains(e.target as Node)) return;
+      handler();
+    };
+
+    document.addEventListener("mousedown", listener);
+    return () => document.removeEventListener("mousedown", listener);
+  }, [ref, handler]);
+}
